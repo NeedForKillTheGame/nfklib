f229d96029784040c218928fce353e1088437a05
HarpyWar <harpywar@gmail.com>
2014-10-18 18:17:36 +0400

    update test program to display demo stats

--- test/Program.cs
+++ test/Program.cs
@@ -10,16 +10,62 @@ namespace test
     {
         static void Main(string[] args)
         {
-            var fileName = @"D:\Games\NFK\basenfk\demos\54734_HarpyWar_&_S^2ElekTroN_vs_mne_vezet_ya_igrau_&_megarchi_on_ctf1_CTF_20-59-04.ndm";
-            //var fileName = "ctf2.mapa";
-            var ndm = new nfklib.NDemo.NFKDemo();
-            var demo = ndm.Read(fileName);
-
-            if (demo != null)
+            if (args.Length != 1)
             {
-                Console.WriteLine("Map size: {0}x{1}", demo.Map.Header.MapSizeX, demo.Map.Header.MapSizeY);
-                Console.WriteLine("Players: {0}, Stats: {1}", demo.Players.Count, demo.PlayerStats.Count);
+                Console.WriteLine("Usage: test.exe demo.ndm");
+                Environment.Exit(0);
             }
+            var fileName = args[0];
+
+            var ndm = new nfklib.NDemo.NFKDemo();
+            try
+            {
+                var demo = ndm.Read(fileName);
+                if (demo != null)
+                {
+                    Console.WriteLine("Map: {0} ({1}x{2})", Helper.GetDelphiString(demo.Map.Header.MapName), demo.Map.Header.MapSizeX, demo.Map.Header.MapSizeY);
+                    Console.WriteLine("Demo duration: {0} seconds", demo.Duration);
+                    Console.WriteLine();
+                    foreach (var p in demo.Players)
+                    {
+                        Console.WriteLine("---------------------------------------------");
+                        Console.WriteLine("Stats for {0} ({1})", Helper.GetRealNick(Helper.GetDelphiString(p.netname)), Helper.GetDelphiString(p.modelname));
+                        Console.WriteLine();
+                        foreach (var s in demo.PlayerStats)
+                        {
+                            if (p.DXID == s.DXID)
+                            {
+                                Console.WriteLine("kills: {0}, deaths: {1}, suicides: {2}, frags: {3}", s.stat_kills, s.stat_deaths, s.stat_suicide, s.frags);
+                                Console.WriteLine();
+                                Console.WriteLine("Accuracy info:");
+                                Console.WriteLine("  Gaunlet:\t{0}", s.gaun_hits);
+                                Console.WriteLine("  Machine:\t{0}/{1}\t{2:0}%", s.mach_hits, s.mach_fire, s.mach_hits / ((double)s.mach_fire / 100));
+                                Console.WriteLine("  Shotgun:\t{0}/{1}\t{2:0}%", s.shot_hits, s.shot_fire, s.shot_hits / ((double)s.shot_fire / 100));
+                                Console.WriteLine("  Grenade:\t{0}/{1}\t{2:0}%", s.gren_hits, s.gren_fire, s.gren_hits / ((double)s.gren_fire / 100));
+                                Console.WriteLine("  Rocket:\t{0}/{1}\t{2:0}%", s.rocket_hits, s.rocket_fire, s.rocket_hits / ((double)s.rocket_fire / 100));
+                                Console.WriteLine("  Shaft:\t{0}/{1}\t{2:0}%", s.shaft_hits, s.shaft_fire, s.shaft_hits / ((double)s.shaft_fire / 100));
+                                Console.WriteLine("  Rail:\t\t{0}/{1}\t{2:0}%", s.rail_hits, s.rail_fire, s.rail_hits / ((double)s.rail_fire / 100));
+                                Console.WriteLine("  Plazma:\t{0}/{1}\t{2:0}%", s.plasma_hits, s.plasma_fire, s.plasma_hits / ((double)s.plasma_fire / 100));
+                                Console.WriteLine();
+                                Console.WriteLine("dmggiven\t{0}", s.stat_dmggiven);
+                                Console.WriteLine("dmgreceived\t{0}", s.stat_dmgrecvd);
+                                Console.WriteLine();
+                                Console.WriteLine("Rewards:");
+                                Console.WriteLine("  Impressive: {0}", s.bonus_impressive);
+                                Console.WriteLine("  Excellent: {0}", s.bonus_excellent);
+                                Console.WriteLine("  Humiliation: {0}", s.bonus_humiliation);
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
         }
     }
 }

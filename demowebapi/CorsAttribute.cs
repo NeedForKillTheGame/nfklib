f312ed657aa803c30a0849c8dd089b1d7b52152b
HarpyWar <harpywar@gmail.com>
2019-02-04 21:44:22 +0300

    add cors attribute to allow browsers use this method directly with js

---
+++ demowebapi/CorsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace demowebapi
+{
+    class CorsAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+        
+    }
+}
